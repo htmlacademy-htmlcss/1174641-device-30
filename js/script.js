32c4de9a4b0502e0cce74a97e2b58cfff1a21f88
Инна <i.m.makhova@gmail.com>
2021-02-23 17:28:28 +0200

    слайдер преимуществ компании на js

--- js/script.js
+++ js/script.js
@@ -27,22 +27,36 @@ for (let i = 0; i < buttonsClose.length; i++) {
 }
 
 //слайдер популярных товаров
-let slides = document.querySelectorAll('.popular-slide-item');
+let slidesPopular = document.querySelectorAll('.popular-slide-item');
 let slideControls = document.querySelectorAll('.slide-control');
 for (let i = 0; i < slideControls.length; i++) {
   slideControls[i].addEventListener('click', function() {
     if (!slideControls[i].classList.contains('slide-check')) {
       for (let j = 0; j < slideControls.length; j++) {
         slideControls[j].classList.remove('slide-check');
-        slides[j].classList.remove('popular-slide-show');
+        slidesPopular[j].classList.remove('popular-slide-show');
       }
     slideControls[i].classList.add('slide-check');
-    slides[i].classList.add('popular-slide-show');
+    slidesPopular[i].classList.add('popular-slide-show');
     }
   });
 }
 
 //слайдер преимуществ компании
-let
+let slidesAdvantages = document.querySelectorAll('.advantages-slide');
+let slideAdvantagesWrappers = document.querySelectorAll('.button-wrap');
+let slideAdvantagesControls = document.querySelectorAll('.advantages-button');
+for (let i = 0; i < slideAdvantagesControls.length; i++) {
+  slideAdvantagesControls[i].addEventListener('click', function() {
+    if (!slideAdvantagesWrappers[i].classList.contains('current')) {
+      for (let j = 0; j < slideAdvantagesWrappers.length; j++) {
+        slideAdvantagesWrappers[j].classList.remove('current');
+        slidesAdvantages[j].classList.remove('show');
+      }
+    slideAdvantagesWrappers[i].classList.add('current');
+    slidesAdvantages[i].classList.add('show');
+    }
+  });
+}
 
 
